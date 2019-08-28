88222e5865daea52de96fee48261c21f14ce97b2
Matheus Saraiva <34166879+osaraivamatheus@users.noreply.github.com>
2019-08-28 11:02:22 -0300

    Add files via upload

---
+++ adverse_selection.R
@@ -0,0 +1,259 @@
+# DADOS -------------------------------------------------------------------
+setwd('/home/matheus/Dropbox/TCC_R/dados/')
+library(ggplot2); library(reshape2)
+list.files()
+dados = read.csv('dados.csv')
+names(dados)
+ano = function(vetor){
+  retval = as.POSIXlt(as.Date(vetor))$year + 1900
+  return(retval)
+}
+mes = function(vetor){
+  retval = as.POSIXlt(as.Date(vetor))$mon + 1
+  return(retval)  
+}
+summary(dados)
+dados$RISCO[is.na(dados$RISCO)] = 0
+# PARAMETROS POPULACIONAIS ------------------------------------------------
+m1 = .750 #probabilidades de perda da populacao de alto risco
+m2 = .5 #probabilidades de perda da populacao de baixo risco
+lambda = -20 #parametro de demanda (propensao de entrada de pessoas na carteira) 
+tau = 1 #especialidade de demanda (ver HAO,2015)
+
+# SELECAO ADVSERSA E LOSS COVERAGE --------------------------------------------------------
+advsl_lc = function(m1,m2,tau,lambda){
+  risk_H = dados$COD_USR[dados$RISCO >= m1]
+  risk_L = dados$COD_USR[dados$RISCO < m2]
+  
+  p1 = length(risk_H) / length(dados$COD_USR)
+  p2 = length(risk_L) / length(dados$COD_USR)
+  
+  pi1 = m1*p1
+  pi2 = m2*p2
+  
+  expected_loss = (m1*p1) + (m2*p2)
+  
+  demand_function = function(lambda){
+    tau = 1
+    d1 = tau*(mean(dados$premio_medio[dados$COD_USR %in% risk_H]) / mean(dados$RISCO[dados$COD_USR %in% risk_H]))**-lambda
+    d2 = tau*(mean(dados$premio_medio[dados$COD_USR %in% risk_L]) / mean(dados$RISCO[dados$COD_USR %in% risk_L]))**-lambda
+    return(c(d1,d2))
+  }
+  
+  expected_coverage = demand_function(lambda)[1]*p1*pi1 + demand_function(lambda)[2]*p2*pi2
+  expected_loss_coverage = demand_function(lambda)[1]*p1*pi1*m1 + demand_function(lambda)[2]*p2*pi2*m2
+  
+  rho = expected_loss_coverage/expected_coverage
+  
+  as = rho / expected_loss
+  
+  lc = c(expected_loss_coverage, expected_loss)
+  
+  advsl = c(as, expected_coverage, expected_loss_coverage, rho)
+  
+  return(list(advsl, lc))
+}
+
+# TAXA DE SELECAO ADVERSA -------------------------------------------------
+adverse_selection_ratio = function(m1,m2,tau,lambda){
+  risk_L = dados$COD_USR[dados$RISCO <= m1]
+  risk_H = dados$COD_USR[dados$RISCO >= m2]
+  
+  p1 = length(risk_L) / length(dados$COD_USR)
+  p2 = length(risk_H) / length(dados$COD_USR)
+  
+  pi1 = m1*p1
+  pi2 = m2*p2
+  
+  expected_loss = (m1*p1) + (m2*p2)
+  
+  demand_function = function(lambda){
+    tau = 1
+    d1 = tau*(mean(dados$premio_medio[dados$COD_USR %in% risk_L]) / mean(dados$RISCO[dados$COD_USR %in% risk_L]))**-lambda
+    d2 = tau*(mean(dados$premio_medio[dados$COD_USR %in% risk_H]) / mean(dados$RISCO[dados$COD_USR %in% risk_H]))**-lambda
+    alfa1 = (tau*p1)/(tau*p1 + tau*p2)
+    alfa2 = (tau*p2)/(tau*p1 + tau*p2)
+    return(c(d1,d2,alfa1,alfa2))
+  }
+  
+  expected_coverage = demand_function(lambda)[1]*p1*pi1 + demand_function(lambda)[2]*p2*pi2
+  expected_loss_coverage = demand_function(lambda)[1]*p1*pi1*m1 + demand_function(lambda)[2]*p2*pi2*m2
+  
+  rho = expected_loss_coverage/expected_coverage
+  
+  as_ratio = rho / (demand_function(lambda)[3]*m1 + demand_function(lambda)[4]*m2)
+  
+  return(as_ratio)
+}
+
+# FUNCOES DE DEMANDA ------------------------------------------------------
+demand_function_premio =function(m1,m2 = (1-m1),tau,lambda, premio1,premio2){
+  risk_H = dados$COD_USR[dados$RISCO >= m1]
+  risk_L = dados$COD_USR[dados$RISCO < m2]
+  
+  p1 = length(risk_H) / length(dados$COD_USR)
+  p2 = length(risk_L) / length(dados$COD_USR)
+  
+  pi1 = m1*p1
+  pi2 = m2*p2
+
+  tau = 1
+  d1 = tau*(premio1 / mean(dados$RISCO[dados$COD_USR %in% risk_H]))**-lambda
+  d2 = tau*(premio2 / mean(dados$RISCO[dados$COD_USR %in% risk_L]))**-lambda
+  alfa1 = (tau*p1)/(tau*p1 + tau*p2)
+  alfa2 = (tau*p2)/(tau*p1 + tau*p2)
+  return(c(d1,d2,alfa1,alfa2))
+}  #DEMANDA EM FUNCAO DO PREMIO
+demand_function_lambda =function(m1,m2 = (1-m1),tau,lambda){
+  risk_H = dados$COD_USR[dados$RISCO >= m1]
+  risk_L = dados$COD_USR[dados$RISCO < m2]
+  
+  p1 = length(risk_H) / length(dados$COD_USR)
+  p2 = length(risk_L) / length(dados$COD_USR)
+  
+  pi1 = m1*p1
+  pi2 = m2*p2
+  
+  expected_loss = (m1*p1) + (m2*p2)
+  
+  tau = 1
+  d1 = tau*(mean(dados$premio_medio[dados$COD_USR %in% risk_H]) / mean(dados$RISCO[dados$COD_USR %in% risk_H]))**-lambda
+  d2 = tau*(mean(dados$premio_medio[dados$COD_USR %in% risk_L]) / mean(dados$RISCO[dados$COD_USR %in% risk_L]))**-lambda
+  alfa1 = (tau*p1)/(tau*p1 + tau*p2)
+  alfa2 = (tau*p2)/(tau*p1 + tau*p2)
+  return(c(d1,d2,alfa1,alfa2))
+} #DEMANDA EM FUNCAO DO LAMBDA
+demand_function_risco =function(m1,m2 = (1-m1),tau,lambda){
+  risk_H = dados$COD_USR[dados$RISCO >= m1]
+  risk_L = dados$COD_USR[dados$RISCO < m2]
+  
+  p1 = length(risk_H) / length(dados$COD_USR)
+  p2 = length(risk_L) / length(dados$COD_USR)
+  
+  pi1 = m1*p1
+  pi2 = m2*p2
+  
+  tau = 1
+  d1 = tau*(mean(dados$premio_medio[dados$COD_USR %in% risk_H]) / mean(dados$RISCO[dados$COD_USR %in% risk_H]))**-lambda
+  d2 = tau*(mean(dados$premio_medio[dados$COD_USR %in% risk_L]) / mean(dados$RISCO[dados$COD_USR %in% risk_L]))**-lambda
+  alfa1 = (tau*p1)/(tau*p1 + tau*p2)
+  alfa2 = (tau*p2)/(tau*p1 + tau*p2)
+  return(c(d1,d2,alfa1,alfa2))
+} #DEMANDA EM FUNCAO DO RISCO
+
+
+# FUNCOES DE PLOTAGEM --------------------------------------------
+plot_advs = function(m1,m2,lambda){
+  d = data.frame(matrix(NA, nrow = 100, ncol = 3))
+  names(d) = c('m1','m2','as')
+  
+  d$m1 = seq(m1,1, length.out = 100)
+  d$m2 = seq(0,m2, length.out = 100)
+  
+  for (i in 1:100) {
+    d$as[i] = advsl_lc(d$m1[i], d$m2[i], tau, lambda)[[1]][1]
+  }
+  
+  plot(d$m1, d$as, t = 'l', lwd = 1, 
+       main = paste0('Alto risco >= ', m1, ', baixo risco < ', m2, ', lambda = ', lambda), xlab = TeX('$\\mu_{1}$'), ylab = 'advs',  cex.main=0.81)
+  abline(h = 1, col  = 'red', lty = 'dotted')
+}
+plot_demanda_premio = function(m1, lambda){
+  demanda = data.frame(matrix(NA, nrow = 100, 4))
+  names(demanda) = c('premio1','premio2','d1','d2')
+  demanda$premio2 = seq(0.01,0.40,length.out = 100)
+  demanda$premio1 = seq(0.65,1,length.out = 100)
+  for (i in 1:100) {
+    demanda$d1[i] = demand_function_premio(m1,(1-m1),1,lambda,demanda$premio1[i],demanda$premio2[i])[1]
+    demanda$d2[i] = demand_function_premio(m1,(1-m1),1,lambda,demanda$premio1[i],demanda$premio2[i])[2]
+  }
+  
+  par(mfrow = c(1,2))
+  plot(demanda$premio1, demanda$d1, t = 'l', xlab = expression(Pi[1]), ylab = expression(d[1]), main = 'Demanda em função do prêmio')
+  mtext(bquote(mu[1]==.(m1)~lambda == .(lambda)), side = 3, line = 0)
+  plot(demanda$premio2, demanda$d2, t = 'l', xlab = expression(Pi[2]), ylab = expression(d[2]), main = 'Demanda em função do prêmio')
+  mtext(bquote(mu[1]==.(m1)~lambda == .(lambda)), side = 3, line = 0)
+  
+} #DEMANDA EM FUNCAO DO PREMIO
+plot_demanda_lambda = function(m1){
+  demanda = data.frame(matrix(NA, nrow = 100, 3))
+  names(demanda) = c('lambda','d1','d2')
+  demanda$lambda = seq(-1,1,length.out = 100)
+  for (i in 1:100) {
+    demanda$d1[i] = demand_function_lambda(m1,(1-m1),1,demanda$lambda[i])[1]
+    demanda$d2[i] = demand_function_lambda(m1,(1-m1),1,demanda$lambda[i])[2]
+  }
+  
+  plot(demanda$lambda, demanda$d1, t = 'l', xlab = expression(lambda), ylab = 'demanda', main = bquote(.('Demanda em função de ')~lambda))
+  lines(demanda$lambda, demanda$d2, col = 'red')
+  text(x=-0.58, y=150, expression(d[1]), col = "red")
+  text(x=-1, y=150, expression(d[2]), col = "black")
+  mtext(bquote(mu[1]==.(m1)), side = 3, line = 0)
+  
+} #DEMANDA EM FUNCAO DO LAMBDA
+plot_demanda_risco = function(lambda){
+  demanda = data.frame(matrix(NA, nrow = 100, 4))
+  names(demanda) = c('risco_alto','risco_baixo','d1','d2')
+  demanda$risco_alto = seq(0.60,0.95,length.out = 100)
+  demanda$risco_baixo = 1-demanda$risco_alto
+  for (i in 1:100) {
+    demanda$d1[i] = demand_function_risco(demanda$risco_alto[i],demanda$risco_baixo[i],1,lambda)[1]
+    demanda$d2[i] = demand_function_risco(demanda$risco_alto[i],demanda$risco_baixo[i],1,lambda)[2]
+  }
+  
+  par(mfrow = c(1,2))
+  plot(demanda$risco_alto, demanda$d1, t = 'l', xlab = expression(mu[1]), ylab = expression(d[1]), main = 'Demanda em função do risco')
+  mtext(bquote(lambda==.(lambda)), side = 3, line = 0)
+  plot(demanda$risco_baixo, demanda$d2, t = 'l', xlab = expression(mu[2]), ylab = expression(d[2]), main = 'Demanda em função do risco')
+  mtext(bquote(lambda==.(lambda)), side = 3, line = 0)
+} #DEMANDA EM FUNCAO DO RISCO
+
+# GRAFICOS DE DEMANDA EM FUNCAO DO LAMBDA -----------------------------------------------------
+plot_demanda_lambda(m1 = .65)
+# GRAFICOS DE DEMANDA EM FUNCAO DO PREMIO -----------------------------------------------------
+plot_demanda_premio(m1 = .65,lambda = -0.5)
+
+# GRAFICOS DE DEMANDA EM FUNCAO DO RISCO -----------------------------------------------------
+plot_demanda_risco(lambda = -0.5)
+  
+# GRAFICOS TAXA DE SELECAO ADVERSA ----------------------------------------------------------------
+  d = data.frame(matrix(NA, nrow = 20, ncol = 4))
+  names(d) = c('lambda',paste0('ratio_advsl_C',1:3))
+  
+  d$lambda = seq(0,5.5, length.out = 20)
+  m1 = c(.01,.04,.07)
+  
+  for (i in 1:20) {
+    d$ratio_advsl_C1[i] = adverse_selection_ratio(m1[1],m1[1]*6,tau,d$lambda[i])
+    d$ratio_advsl_C2[i] = adverse_selection_ratio(m1[2],m1[2]*6,tau,d$lambda[i])
+    d$ratio_advsl_C3[i] = adverse_selection_ratio(m1[3],m1[3]*6,tau,d$lambda[i])
+  }
+  
+  labs = c(expression(paste(mu[1] == .01,' ,', ~ mu[2] == 0.06)),
+           expression(paste(mu[1] == .04,' ,', ~ mu[2] == 0.24)),
+           expression(paste(mu[1] == .07,' ,', ~ mu[2] == 0.42)))
+  
+  ggplot(melt(d, id.vars = 'lambda', value.name = 'ratio', variable.name = 'cenario'), 
+    aes(lambda, ratio, group = cenario)) + 
+    # geom_line(aes(linetype = cenario)) + 
+     geom_line() +
+     geom_point(aes(shape = cenario), size = 4) + scale_shape_manual(values = c(15,16,17), labels=labs) +
+     scale_linetype_manual(name = 'Cenário', labels=labs, values=c("solid", 'twodash', 'dotted')) +
+    theme_bw() + 
+    theme(axis.line = element_line(size=1, colour = "black"),
+          panel.grid.major = element_line(), panel.grid.minor = element_blank(),
+          panel.border = element_blank(), panel.background = element_blank(), 
+          axis.title.x = element_text(size = 16), axis.text.x = element_text(size = 16, color ='black'),
+          axis.title.y = element_text(size = 16), axis.text.y = element_text(size = 16, color ='black'),
+    legend.title = element_blank(),
+    legend.text = element_text(size = 16, color ='black'),
+    legend.key.size = unit(1, 'cm'))+
+    xlab(expression(lambda)) + ylab('Taxa de seleção adversa')  +
+    geom_hline(yintercept = c(max(d$ratio_advsl_C1),max(d$ratio_advsl_C2),max(d$ratio_advsl_C3)), col = 'red', linetype = 'dashed', size = .7) +
+    geom_text(x=5.68, y=max(d$ratio_advsl_C1)-.01, label=as.character(round(max(d$ratio_advsl_C1),2)), col = 'red', family = 'mono') +
+    geom_text(x=5.68, y=max(d$ratio_advsl_C2)-.01, label=as.character(round(max(d$ratio_advsl_C2),2)), col = 'red', family = 'mono') +
+    geom_text(x=5.68, y=max(d$ratio_advsl_C3)-.01, label=as.character(round(max(d$ratio_advsl_C3),2)), col = 'red', family = 'arial')
+
+    
+  
+
---
+++ loss_coverage.R
@@ -0,0 +1,279 @@
+# PREPARACAO DE DADOS -----------------------------------------------------
+setwd('/home/matheus/Dropbox/TCC_R/dados/')
+library(ggplot2); library(reshape2); library(gridExtra);library(xtable)
+list.files()
+prop = read.csv('proporcoes.csv', sep = ';', dec = ',', header = T)
+dados_risco = read.csv('dados.csv')
+dados_risco$RISCO[is.na(dados_risco$RISCO)] = 0
+names(dados_risco)
+ano = function(vetor){
+  retval = as.POSIXlt(as.Date(vetor))$year + 1900
+  return(retval)
+}
+mes = function(vetor){
+  retval = as.POSIXlt(as.Date(vetor))$mon + 1
+  return(retval)  
+}
+russam = read.table('fator_rusam.txt')
+names(russam)[2:8] = 2011:2017
+russam$RELATIVIZADO = russam$f.bar/max(russam$f.bar)
+razdep_bh_2000 = read.csv('razdep-bh.csv', sep = ';',dec = ','); razdep_bh_2000[,2] = razdep_bh_2000[,2]/100
+razdep_bh_2010 = prop[10,3] / sum(prop[c(1:9),3])
+var_razdep_idosos = razdep_bh_2010 / razdep_bh_2000[2,2]
+
+faixas_atuais = c('0 a 18 anos', "19 a 23 anos", "24 a 28 anos","29 a 33 anos", "34 a 38 anos", "39 a 43 anos","44 a 48 anos", "49 a 53 anos", "54 a 58 anos","59 anos ou +")
+f1 = 0:18; f2 = 19:23 ; f3 = 24:28; f4= 29:33 ; f5= 34:38; f6 = 39:43 ; f7 = 44:48; f8 = 49:53; f9 = 54:58; f10 = 59:160
+prop[,c(6:8)] = prop[,c(6:8)]/100
+prop$DSC_FAIXA_ETARIA = rep(faixas_atuais,2)
+
+## ADICIONANDO FAIXAS ETARIAS NOS DADOS
+for (contador_faixa in 1:10) {
+  dados_risco$DSC_FAIXA_ETARIA[(2017 - ano(dados_risco$DAT_NASC)) %in% eval(parse(text = paste0('f',contador_faixa)))] = faixas_atuais[contador_faixa]
+  russam$DSC_FAIXA_ETARIA[russam$Idade %in% eval(parse(text = paste0('f',contador_faixa)))] = faixas_atuais[contador_faixa]
+  cat('faixa: ',faixas_atuais[contador_faixa],'\n')
+}
+
+## CALCULANDO RISCO MEDIO DOS DADOS POR FAIXA
+for (faixa in faixas_atuais) {
+  prop$PREMIO_MEDIO[prop$DSC_FAIXA_ETARIA == faixa & prop$LOCAL == 'BH'] = mean(dados_risco$premio_medio[dados_risco$DSC_FAIXA_ETARIA == faixa], na.rm = T)
+  prop$RISCO_ALFA_MEDIO[prop$DSC_FAIXA_ETARIA == faixa & prop$LOCAL == 'BH'] = mean(dados_risco$RISCO[dados_risco$DSC_FAIXA_ETARIA == faixa], na.rm = T)
+  prop$RISCO_RUSSAM_MEDIO[prop$DSC_FAIXA_ETARIA == faixa & prop$LOCAL == 'BH'] = mean(russam$RELATIVIZADO[russam$DSC_FAIXA_ETARIA == faixa], na.rm = T)
+}
+
+
+# GRAFICOS DESCRITIVOS ----------------------------------------------------
+p = ggplot(prop[prop$LOCAL == 'BH',], aes(DSC_FAIXA_ETARIA, PREMIO_MEDIO, group = 1)) + geom_point() + geom_line() + theme_bw() + theme(axis.line = element_line(size=1, colour = "black"),panel.grid.major = element_line(), panel.grid.minor = element_blank(),panel.border = element_blank(), panel.background = element_blank(),axis.title.x = element_text(size = 10,color = 'black'), axis.text.x = element_text(size = 10, angle = 0, vjust = .55,color = 'black'),axis.title.y = element_text(size = 10,color = 'black'), axis.text.y = element_text(size = 10, color = 'black')) + xlab('')+  ylab('Prêmio médio')
+rR = ggplot(prop[prop$LOCAL == 'BH',], aes(DSC_FAIXA_ETARIA, RISCO_RUSSAM_MEDIO, group = 1)) + geom_point() + geom_line() + theme_bw() + theme(axis.line = element_line(size=1, colour = "black"),panel.grid.major = element_line(), panel.grid.minor = element_blank(),panel.border = element_blank(), panel.background = element_blank(),axis.title.x = element_text(size = 10,color = 'black'), axis.text.x = element_text(size = 10, angle = 0, vjust = .55,color = 'black'),axis.title.y = element_text(size = 10,color = 'black'), axis.text.y = element_text(size = 10, color = 'black')) + xlab('')+  ylab('Russam médio')
+rA = ggplot(prop[prop$LOCAL == 'BH',], aes(DSC_FAIXA_ETARIA, RISCO_ALFA_MEDIO, group = 1)) + geom_point() + geom_line() + theme_bw() + theme(axis.line = element_line(size=1, colour = "black"),panel.grid.major = element_line(), panel.grid.minor = element_blank(),panel.border = element_blank(), panel.background = element_blank(),axis.title.x = element_text(size = 10,color = 'black'), axis.text.x = element_text(size = 10, angle = 0, vjust = .55,color = 'black'),axis.title.y = element_text(size = 10,color = 'black'), axis.text.y = element_text(size = 10, color = 'black')) + xlab('')+  ylab(expression(bar(alpha)))
+grid.arrange(p, rR, rA, ncol = 1)
+
+
+# FUNCAO LOSS COVERAGE PARA 10 FAIXAS ETARIAS -----------------------------
+LC = function(vetor_premio,vetor_risco_bh,tau,lambda){
+  vetor_risco_br = prop$TOTAL_PROP[prop$LOCAL == 'Brasil']
+  p = vetor_risco_bh
+  risk = numeric()
+  risk[1] = length(dados_risco$COD_USR[dados_risco$RISCO <= vetor_risco_br[1]]) / length(dados_risco$COD_USR)
+  for (i in 2:10) {
+    risk[i] = length(dados_risco$COD_USR[dados_risco$RISCO > risk[(i-1)] & dados_risco$RISCO < vetor_risco_br[i]]) / length(dados_risco$COD_USR)
+  }
+  pii = vetor_risco_br * vetor_premio
+  
+  demand_function = function(lambda){
+    tau = 1
+    d = tau*(vetor_premio/p)**-lambda
+    return(d)
+  }
+  
+  expected_loss_coverage = sum(demand_function(lambda)*p*pii*vetor_risco_br)
+  
+  lc = expected_loss_coverage
+  
+  return(lc)
+}
+# CRIANDO REGRAS --------------------------------------------------------
+prop$REGRA_1 = NA
+prop$REGRA_2 = NA
+prop$REGRA_3 = NA
+prop$REGRA_4 = NA
+prop$REGRA_5 = NA
+
+  #---------REGRA 1: f10 <= variacao razdep idosos * f1
+  prop$REGRA_1[1:10] = seq(min(prop$PREMIO_MEDIO[1:10]), var_razdep_idosos*max(prop$PREMIO_MEDIO[1:10]), length.out = 10)
+
+  #---------REGRA 2: variacao acumulada entre f1 e f7 < variacao acumulada entre f7 e f10
+  prop$REGRA_2[1:10] = c(prop$PREMIO_MEDIO[1:7]*1.9, prop$PREMIO_MEDIO[8:10])
+
+  #---------REGRA 3: variacao acumulada entre f1 e f7 < variacao acumulada entre f7 e f10
+  prop$REGRA_3[1:10] = c(prop$PREMIO_MEDIO[1],rep(prop$PREMIO_MEDIO[2],8),prop$PREMIO_MEDIO[10])
+  
+  #---------REGRA 4:
+  prop$REGRA_4[1:10] = rep(mean(prop$PREMIO_MEDIO[1:10]),10)
+  
+  #---------REGRA 5:
+  prop$REGRA_5[1:10] = 1000*prop$RISCO_RUSSAM_MEDIO[1:10]
+
+
+
+
+# RESULTADOS PARA DIFERENTES REGRAS -------------------------------------
+
+tabela_latex = function(l){
+# RESULTADOS PARA DIFERENTES REGRAS - UTILIZANDO RISCO ALFA E LAMBDA = l-------------------------------------
+LC_REGRA_ANS = LC(vetor_premio = prop$PREMIO_MEDIO[1:10],vetor_risco = prop$RISCO_ALFA_MEDIO[1:10],tau = 1,lambda = l)
+LC_REGRA_1 = LC(vetor_premio = prop$REGRA_1[1:10],vetor_risco = prop$RISCO_ALFA_MEDIO[1:10],tau = 1,lambda = l)
+LC_REGRA_2 = LC(vetor_premio = prop$REGRA_2[1:10],vetor_risco = prop$RISCO_ALFA_MEDIO[1:10],tau = 1,lambda = l) 
+LC_REGRA_3 = LC(vetor_premio = prop$REGRA_3[1:10],vetor_risco = prop$RISCO_ALFA_MEDIO[1:10],tau = 1,lambda = l) 
+LC_REGRA_4 = LC(vetor_premio = prop$REGRA_4[1:10],vetor_risco = prop$RISCO_ALFA_MEDIO[1:10],tau = 1,lambda = l) 
+LC_REGRA_5 = LC(vetor_premio = prop$REGRA_5[1:10],vetor_risco = prop$RISCO_ALFA_MEDIO[1:10],tau = 1,lambda = l)
+alfa = data.frame(LC_REGRA_ANS,LC_REGRA_1, LC_REGRA_2,LC_REGRA_3,LC_REGRA_4, LC_REGRA_5)
+# RESULTADOS PARA DIFERENTES REGRAS - UTILIZANDO RISCO RUSSAM E LAMBDA = l-------------------------------------
+LC_REGRA_ANS = LC(vetor_premio = prop$PREMIO_MEDIO[1:10],vetor_risco = prop$RISCO_RUSSAM_MEDIO[1:10],tau = 1,lambda = l)
+LC_REGRA_1 = LC(vetor_premio = prop$REGRA_1[1:10],vetor_risco = prop$RISCO_RUSSAM_MEDIO[1:10],tau = 1,lambda = l) 
+LC_REGRA_2 = LC(vetor_premio = prop$REGRA_2[1:10],vetor_risco = prop$RISCO_RUSSAM_MEDIO[1:10],tau = 1,lambda = l) 
+LC_REGRA_3 = LC(vetor_premio = prop$REGRA_3[1:10],vetor_risco = prop$RISCO_RUSSAM_MEDIO[1:10],tau = 1,lambda = l) 
+LC_REGRA_4 = LC(vetor_premio = prop$REGRA_4[1:10],vetor_risco = prop$RISCO_RUSSAM_MEDIO[1:10],tau = 1,lambda = l) 
+LC_REGRA_5 = LC(vetor_premio = prop$REGRA_5[1:10],vetor_risco = prop$RISCO_RUSSAM_MEDIO[1:10],tau = 1,lambda = l) 
+russam = data.frame(LC_REGRA_ANS,LC_REGRA_1, LC_REGRA_2,LC_REGRA_3,LC_REGRA_4, LC_REGRA_5)
+
+return(xtable(data.frame(rbind(alfa,russam), row.names = c('risco alfa','risco russam')),
+              caption = paste0('lambda = ',l), digits = 4))
+}
+
+tabela_latex(0.5)
+# GRAFICOS NAO ANCORADOS PELA REGRA ANS ----------------------------------------------------------------
+#   ALFA 
+names(prop)[9] = 'REGRA_ANS'
+d = data.frame(matrix(NA, nrow = 900,3))
+names(d) = c('Regra','Lambda','LC')
+d$Regra = rep(c('REGRA_ANS', paste0('REGRA_',1:5)),each = 150)
+d$Lambda = rep(seq(.1,.9,length.out = 150),6)
+
+for (regra in c('REGRA_ANS', paste0('REGRA_',1:5))) {
+  for (lambda in seq(.1,.9,length.out = 150)) {
+    d$LC[d$Regra == regra & d$Lambda == lambda] = 
+      eval(parse(text = paste0(
+        'LC(vetor_premio = prop$',regra,'[1:10],vetor_risco = prop$RISCO_ALFA_MEDIO[1:10],tau = 1,lambda = ',lambda,')'
+      )))
+  }
+  cat('regra: ', regra,' \n')
+  
+  }
+
+
+#RUSSAM 
+names(prop)[9] = 'REGRA_ANS'
+r = data.frame(matrix(NA, nrow = 900,3))
+names(r) = c('Regra','Lambda','LC')
+r$Regra = rep(c('REGRA_ANS', paste0('REGRA_',1:5)),each = 150) 
+r$Lambda = rep(seq(.1,.9,length.out = 150),6)
+
+for (regra in c('REGRA_ANS', paste0('REGRA_',1:5))) {
+  for (lambda in seq(.1,.9,length.out = 150)) {
+    r$LC[r$Regra == regra & r$Lambda == lambda] = 
+      eval(parse(text = paste0(
+        'LC(vetor_premio = prop$',regra,'[1:10],vetor_risco = prop$RISCO_RUSSAM_MEDIO[1:10],tau = 1,lambda = ',lambda,')'
+      )))
+    
+  }
+  cat('regra: ', regra,' \n')
+}
+
+d = rbind(d, r)
+d$Risco = rep(c('alfa','russam'), each = 900)
+
+
+G_ALFA = ggplot(d[d$Risco == 'alfa',], aes(Lambda, LC, color = Regra)) + geom_line() +
+  theme_bw()  +
+  theme(axis.line = element_line(size=1, colour = "black"),
+        panel.grid.major = element_line(), panel.grid.minor = element_blank(),
+        panel.border = element_blank(), panel.background = element_blank(), 
+        plot.title = element_text(hjust = 0.5),
+        axis.title.x = element_text(size = 16), axis.text.x = element_text(size = 16, color ='black'),
+        axis.title.y = element_text(size = 16), axis.text.y = element_text(size = 16, color ='black'),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 16, color ='black'),
+        legend.key.size = unit(1, 'cm')) +
+  xlab(expression(lambda)) + ylab('LC') + 
+   scale_color_manual(labels = c('Regra ANS',paste0('Regra ',1:5)), values = c('red','blue','black','darkgreen','purple','gray'))
+  # scale_color_brewer(palette = 'Blues')
+
+G_RUSSAM = ggplot(d[d$Risco == 'russam',], aes(Lambda, LC, color = Regra)) + geom_line() +
+  theme_bw()  +
+  theme(axis.line = element_line(size=1, colour = "black"),
+        panel.grid.major = element_line(), panel.grid.minor = element_blank(),
+        panel.border = element_blank(), panel.background = element_blank(), 
+        plot.title = element_text(hjust = 0.5),
+        axis.title.x = element_text(size = 16), axis.text.x = element_text(size = 16, color ='black'),
+        axis.title.y = element_text(size = 16), axis.text.y = element_text(size = 16, color ='black'),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 16, color ='black'),
+        legend.key.size = unit(1, 'cm')) +
+  xlab(expression(lambda)) + ylab('LC') + 
+  scale_color_manual(labels = c('Regra ANS',paste0('Regra ',1:5)), values = c('red','blue','black','darkgreen','purple','gray'))
+
+
+
+
+grid.arrange(G_ALFA, G_RUSSAM, ncol = 2)
+
+
+# GRAFICOS ANCORADOS PELA REGRA ANS ----------------------------------------------------------------
+  #   ALFA 
+names(prop)[9] = 'REGRA_ANS'
+d = data.frame(matrix(NA, nrow = 900,3))
+names(d) = c('Regra','Lambda','LC')
+d$Regra = rep(c('REGRA_ANS', paste0('REGRA_',1:5)),each = 150)
+d$Lambda = rep(seq(.1,.9,length.out = 150),6)
+
+for (regra in c('REGRA_ANS', paste0('REGRA_',1:5))) {
+  for (lambda in seq(.1,.9,length.out = 150)) {
+    d$LC[d$Regra == regra & d$Lambda == lambda] = 
+      eval(parse(text = paste0(
+        'LC(vetor_premio = prop$',regra,'[1:10],vetor_risco = prop$RISCO_ALFA_MEDIO[1:10],tau = 1,lambda = ',lambda,') /
+        LC(vetor_premio = prop$REGRA_ANS[1:10],vetor_risco = prop$RISCO_ALFA_MEDIO[1:10],tau = 1,lambda = ',lambda,')'
+      )))
+  }
+  cat('regra: ', regra,' \n')
+  
+}
+
+
+  #RUSSAM 
+names(prop)[9] = 'REGRA_ANS'
+r = data.frame(matrix(NA, nrow = 900,3))
+names(r) = c('Regra','Lambda','LC')
+r$Regra = rep(c('REGRA_ANS', paste0('REGRA_',1:5)),each = 150) 
+r$Lambda = rep(seq(.1,.9,length.out = 150),6)
+
+for (regra in c('REGRA_ANS', paste0('REGRA_',1:5))) {
+  for (lambda in seq(.1,.9,length.out = 150)) {
+    r$LC[r$Regra == regra & r$Lambda == lambda] = 
+      eval(parse(text = paste0(
+        'LC(vetor_premio = prop$',regra,'[1:10],vetor_risco = prop$RISCO_RUSSAM_MEDIO[1:10],tau = 1,lambda = ',lambda,') /
+        LC(vetor_premio = prop$REGRA_ANS[1:10],vetor_risco = prop$RISCO_RUSSAM_MEDIO[1:10],tau = 1,lambda = ',lambda,')'
+      )))
+    
+  }
+  cat('regra: ', regra,' \n')
+}
+
+d = rbind(d, r)
+d$Risco = rep(c('alfa','russam'), each = 900)
+
+
+G_ALFA = ggplot(d[!(d$Regra == 'REGRA_ANS') & d$Risco == 'russam',], aes(Lambda, LC, color = Regra)) + geom_line() +
+  theme_bw()  +
+  theme(axis.line = element_line(size=1, colour = "black"),
+        panel.grid.major = element_line(), panel.grid.minor = element_blank(),
+        panel.border = element_blank(), panel.background = element_blank(), 
+        plot.title = element_text(hjust = 0.5),
+        axis.title.x = element_text(size = 16), axis.text.x = element_text(size = 16, color ='black'),
+        axis.title.y = element_text(size = 16), axis.text.y = element_text(size = 16, color ='black'),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 16, color ='black'),
+        legend.key.size = unit(1, 'cm'))+ geom_hline(yintercept = 1, col = 'red', linetype = 'dashed', size = 0.8) +
+  xlab(expression(lambda)) + ylab('LC') + scale_color_manual(labels = c(paste0('Regra ',1:5)), values = c('red','blue','black','darkgreen','purple')) +
+  annotate("text", x=0.8, y=1.05, label= "Regra ANS = 1") + 
+  ggtitle(expression('Gráfico utilizando o risco'~alpha))
+
+G_RUSSAM = ggplot(d[!(d$Regra == 'REGRA_ANS') & d$Risco == 'russam',], aes(Lambda, LC, color = Regra)) + geom_line()  +
+  theme_bw()  +
+  theme(axis.line = element_line(size=1, colour = "black"),
+        panel.grid.major = element_line(), panel.grid.minor = element_blank(),
+        panel.border = element_blank(), panel.background = element_blank(), 
+        plot.title = element_text(hjust = 0.5),
+        axis.title.x = element_text(size = 16), axis.text.x = element_text(size = 16, color ='black'),
+        axis.title.y = element_text(size = 16), axis.text.y = element_text(size = 16, color ='black'),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 16, color ='black'),
+        legend.key.size = unit(1, 'cm'))+ geom_hline(yintercept = 1, col = 'red', linetype = 'dashed', size = 0.8) +
+  xlab(expression(lambda)) + ylab('LC') + scale_color_manual(labels = c(paste0('Regra ',1:5)), values = c('red','blue','black','darkgreen','purple')) +
+  annotate("text", x=0.8, y=1.05, label= "Regra ANS = 1") + 
+  ggtitle('Gráfico utilizando o risco Russam')
+
+
+
+
+
+grid.arrange(G_ALFA, G_RUSSAM, ncol = 2)
